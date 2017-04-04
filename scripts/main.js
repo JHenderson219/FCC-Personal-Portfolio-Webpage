ad094acc1ff11390d897960c4a6209a27e85156f
JHenderson219 <hendersonj219@gmail.com>
2017-04-03 17:01:15 -0700

    Complete style on project tiles; style footer links

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const projects = [{
+	name:"Placeholder Project",
+	imageCaption:"This is a placeholder image caption.",
+	imageURL:"https://upload.wikimedia.org/wikipedia/commons/c/c4/PM5544_with_non-PAL_signals.png",
+	projectLink:"#"
+}];
+
+console.log (JSON.stringify(projects));
+function generateProjectTiles(projObj){
+	return projObj.map(function(e){
+		return 
+	});
+}
