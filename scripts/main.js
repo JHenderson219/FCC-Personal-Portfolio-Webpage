925a759cb8c07fbb9ac9055ad85e2dc5ec76cae2
JHenderson219 <hendersonj219@gmail.com>
2017-04-06 16:39:31 -0700

    Clear old script

--- scripts/main.js
+++ scripts/main.js
@@ -12,22 +12,3 @@ function generateProjectTiles(projObj){
 	});
 }
 
-/* Thanks to Glenn Smith on CodePen (https://codepen.io/g13nn/pen/eHGEF) for the jQuery for the hamburger menu */
-//$(".menu").hide();
-//$( ".cross" ).hide();
-
-$( ".hamburger" ).click(function() {
-		console.log(document.getElementsByClassName("menu")[0].style.display)
-		document.getElementsByClassName("menu")[0].style.display = "flex";
-		document.getElementsByClassName("menu")[0].style.position = "absolute";
-		document.getElementsByClassName("menu-link")[0].style.display = "flex";
-		$( ".hamburger" ).hide();
-		$( ".cross" )[0].style.display = "flex";
-
-});
-
-$( ".cross" ).click(function() {
-		$( ".cross" )[0].style.display= "none";
-		document.getElementsByClassName("menu")[0].style.display = "none";
-		$( ".hamburger" )[0].style.display = "flex";
-});
